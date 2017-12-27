7435514f951ca6895d33f753f5de6698a4e3a69a
huzhengen <huzhengen@163.com>
2017-12-27 17:25:16 +0800

    update

--- vue-cli-mt/src/components/Index.vue
+++ vue-cli-mt/src/components/Index.vue
@@ -2,17 +2,20 @@
 	<div class="index">
 		<swipe></swipe>
 		<type-item></type-item>
+		<shop-list></shop-list>
 	</div>
 </template>
 
 <script>
 import Swipe from '@/components/Swipe'
 import TypeItem from '@/components/TypeItem'
+import ShopList from '@/components/ShopList'
 export default{
 	name: 'Index',
 	components: {
 		'swipe': Swipe,
 		'type-item': TypeItem,
+		'shop-list': ShopList,
 	}
 }
 </script>
